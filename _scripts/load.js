df0eb4e9ab72efa73527f6d62d58739cc849e891
Karnezis <ellen_chalegre@hotmail.com>
2020-06-19 23:06:50 -0300

    Função de redimensionamento ajustada.
    Agora, o site redimensiona imagens que são subidas pelo usuário, mostrando seu antes e depois. O redimensionamento não mantém a escala da imagem!

--- _scripts/load.js
+++ _scripts/load.js
@@ -39,13 +39,13 @@ function handleFiles() {
         if (!file.type.match(imageType)) { // Verifica se é uma imagem
             continue;
         }
-
+        
         // Cria um preview da imagem para teste
         var img = document.createElement("img");
         img.classList.add("obj");
         img.file = file;
         preview.appendChild(img);
-
+        
         var reader = new FileReader();
         reader.onload = (function (aImg) { return function (e) { aImg.src = e.target.result; }; })(img);
         reader.readAsDataURL(file);
@@ -53,39 +53,47 @@ function handleFiles() {
 }
 
 function img_resize() { //
-    var files = document.getElementById('dir').files; // Pega os arquivos do input
-    var preview = document.getElementById('file-div'); // Pega o div que testa o resultado
-    for (var i = 0; i < files.length; i++) { // Itera a FileList
-        var file = files[i]; // Recebe um único File da lista
-        var imageType = /image.*/;
+    var arqvs = document.getElementById('dir').files; // Pega os arquivos do input
+    var prev = document.getElementById('file-div'); // Pega o div que testa o resultado
+    for (let i = 0; i < arqvs.length; i++) { // Itera a FileList
+        let file = arqvs[i]; // Recebe um único File da lista
+        let imageType = /image.*/;
 
         if (!file.type.match(imageType)) { // Verifica se é uma imagem
-            continue;
+            continue; // Se não for, pula o redimensionamento.
+        }else{
+            var fileType = file.type;
         }
-
-        //Redimensionando a Imagem
-        var reader = new FileReader();
+        
+        let reader = new FileReader();  // Redimensionando a Imagem
         reader.onloadend = function () {  // Quando tentar carregar uma imagem
-            var image = new Image();  // Faz um novo elemento da classe Imagem
+            let image = new Image();  // Faz um novo elemento da classe Imagem
             image.src = reader.result;  // Pega a imagem do arquivo lido
-
             image.onload = function () {  // Quando carregamos a imagem com sucesso
-
-                var canvas = document.createElement('canvas');  // Cria um Canvas
+                let canvas = document.createElement('canvas');  // Cria um Canvas
                 canvas.width = 224;  // Define a altura do canvas como a desejada
                 canvas.height = 224;  // Define a largura do canvas como a desejada
-
-                var ctx = canvas.getContext("2d");  // Pega o contexto do Canvas
+                let ctx = canvas.getContext("2d");  // Pega o contexto do Canvas
                 ctx.drawImage(this, 0, 0, 224, 224);  // Desenha a imagem no contexto
-                var finalFile = canvas.toDataURL(imageType);  // O arquivo final é jogado no Canvas
-
+                let finalFile = canvas.toDataURL(fileType);  // O arquivo final é jogado no Canvas
                 // Cria um preview da imagem para teste
-                var img = document.createElement("img");
+                let img = document.createElement("img");
                 img.classList.add("obj");
-                img.file = finalFile;
-                preview.appendChild(img);
+                img.src = finalFile;
+                prev.appendChild(image);
+                prev.appendChild(img);
+
+                console.log(`A imagem ${file.name} foi processada. Arquivo ${i} de ${arqvs.length}.`);
             }
         }
         reader.readAsDataURL(file);
+    }
+}
+
+function tensorTest() {
+    var images = document.images;
+    for (let i = 0; i < images.length; i++) {
+        const element = images[i];
+        console.log(`O vetor do Tensorflow é: \n${tf.browser.fromPixels(element)}\n Fim.`);
     }
 }
